33ef835f186b659c82e1cbfa764964908667ec61
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-12-08 01:56:48 +0000

    automatically backed up by learn

--- db/migrate/20171208015221_create_movies.rb
+++ db/migrate/20171208015221_create_movies.rb
@@ -1,4 +1,11 @@
 class CreateMovies < ActiveRecord::Migration
   def change
+    create_table :movies do |t|
+      t.string :title
+      t.datetime :release_date
+      t.string :director
+      t.string :lead_actor
+      t.string :theaters_flag
+    end 
   end
 end
